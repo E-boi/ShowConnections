7fed411f3e655b2a2fc67e15cc139ace7fc1739f
e-boi <hiboysismyoufake6789@gmail.com>
2021-10-13 09:02:21 -0400

    fix

--- Components/Connection.jsx
+++ Components/Connection.jsx
@@ -16,7 +16,11 @@ module.exports = ({ account, pork }) => {
 	return (
 		<TooltipContainer className='sc-connection scrollbarGhostHairline-1mSOM1' text={account.name}>
 			<a target='_blank' href={connection.getPlatformUserUrl?.(account)}>
-				<img alt={Messages.IMG_ALT_LOGO.format({ name: connection?.name })} className={classes.connectedAccountIcon} src={connection?.icon?.color} />
+				<img
+					alt={Messages.IMG_ALT_LOGO.format({ name: connection?.name })}
+					className={classes.connectedAccountIcon}
+					src={connection?.icon?.darkPNG}
+				/>
 				<div className={classes.connectedAccountNameInner}>{account.verified && <Verified className={classes.connectedAccountVerifiedIcon} />}</div>
 			</a>
 		</TooltipContainer>
