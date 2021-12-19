eea09a0bcce2b9245b8c79486f94fb37a6d734d8
e-boi <hiboysismyoufake6789@gmail.com>
2021-12-19 15:50:56 -0500

    fix the fix

--- Components/Connections.jsx
+++ Components/Connections.jsx
@@ -1,24 +1,24 @@
 const { React, getModule } = require('powercord/webpack');
 
 const ConnectedAccount = require('./Connection');
-const { base, muted, uppercase } = getModule(['base'], false);
+const { base, muted, uppercase } = getModule(['base', 'muted'], false);
 const { size12 } = getModule(['size12'], false);
 const { bodyTitle } = getModule(['bodyTitle'], false);
 const { scrollbarGhostHairline } = getModule(['scrollbarGhostHairline'], false);
 
 module.exports = ({ user }) => {
-	const [porkAccounts, setPorkaccounts] = React.useState();
-	const [accounts, setAccounts] = React.useState();
-	if (!accounts && !porkAccounts) {
-		powercord.api.connections.fetchAccounts(user).then(accounts => setPorkaccounts(accounts));
-		getModule(['fetchProfile'], false).fetchProfile(user, 'porkcord', user => setAccounts(user.connected_accounts));
-	}
-	if (accounts?.length <= 0) return null;
-	return [
-		<h3 className={[base, muted, uppercase, size12, bodyTitle].join(' ')}>Connections</h3>,
-		<div className={`sc-connectAccounts ${scrollbarGhostHairline}`}>
-			{accounts?.map(account => account && <ConnectedAccount account={account} />)}
-			{porkAccounts?.map(account => account && <ConnectedAccount account={account} pork />)}
-		</div>,
-	];
+  const [porkAccounts, setPorkaccounts] = React.useState();
+  const [accounts, setAccounts] = React.useState();
+  if (!accounts && !porkAccounts) {
+    powercord.api.connections.fetchAccounts(user).then(accounts => setPorkaccounts(accounts));
+    getModule(['fetchProfile'], false).fetchProfile(user, 'porkcord', user => setAccounts(user.connected_accounts));
+  }
+  if (accounts?.length <= 0) return null;
+  return [
+    <h3 className={[base, muted, uppercase, size12, bodyTitle].join(' ')}>Connections</h3>,
+    <div className={`sc-connectAccounts ${scrollbarGhostHairline}`}>
+      {accounts?.map(account => account && <ConnectedAccount account={account} />)}
+      {porkAccounts?.map(account => account && <ConnectedAccount account={account} pork />)}
+    </div>,
+  ];
 };
