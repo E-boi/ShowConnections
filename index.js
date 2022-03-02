2ee4f363fe673c79c81526f327512ddc5b8ec9a6
e-boi <hiboysismyoufake6789@gmail.com>
2022-03-01 19:39:32 -0500

    untested fix :)

--- index.js
+++ index.js
@@ -2,30 +2,24 @@ const { Plugin } = require('powercord/entities');
 const { getModule, React } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 const Connections = require('./Components/Connections');
+const loadIcons = require('./loadIcons');
 
 module.exports = class ShowConnection extends Plugin {
-	startPlugin() {
-		// e
-		window.webpackChunkdiscord_app.push([
-			[Math.random().toString(36)],
-			{},
-			e => {
-				e(868950);
-			},
-		]);
+  startPlugin() {
+    loadIcons();
 
-		this.loadStylesheet('style.css');
-		const Popout = getModule(m => m.default?.displayName === 'UserPopoutBody', false);
+    this.loadStylesheet('style.css');
+    const Popout = getModule(m => m.default?.displayName === 'UserPopoutBody', false);
 
-		inject('sc-injection', Popout, 'default', ([{ user }], res) => {
-			res.props.children.splice(2, 0, React.createElement(Connections, { user: user.id }));
-			return res;
-		});
+    inject('sc-injection', Popout, 'default', ([{ user }], res) => {
+      res.props.children.splice(2, 0, React.createElement(Connections, { user: user.id }));
+      return res;
+    });
 
-		Popout.default.displayName = 'UserPopoutBody';
-	}
+    Popout.default.displayName = 'UserPopoutBody';
+  }
 
-	pluginWillUnload() {
-		uninject('sc-injection');
-	}
+  pluginWillUnload() {
+    uninject('sc-injection');
+  }
 };
