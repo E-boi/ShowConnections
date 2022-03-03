eeff4dfcce63d186a560694b92d7ca9a84f5cf75
e-boi <hiboysismyoufake6789@gmail.com>
2022-03-02 19:02:32 -0500

    okie it work

--- loadIcons.js
+++ loadIcons.js
@@ -1,13 +1,16 @@
 const { getModule, FluxDispatcher, constants } = require('powercord/webpack');
+const { waitFor } = require('powercord/util');
 
 const { openUserProfileModal, closeUserProfileModal } = getModule(['openUserProfileModal'], false);
 const { getCurrentUser } = getModule(['getCurrentUser', 'getUser'], false);
 
 module.exports = function loadIcons() {
-  if (getModule('connectedAccount', false)) return;
+  if (getModule(['connectedAccount'], false)) return;
   if (!getCurrentUser()) return setTimeout(loadIcons, 100);
+  console.log('hey');
+  console.log(constants.ActionTypes.USER_PROFILE_MODAL_OPEN);
   const e = () => {
-    setTimeout(closeUserProfileModal, 100);
+    waitFor('.root-8LYsGj').then(closeUserProfileModal);
     FluxDispatcher.unsubscribe(constants.ActionTypes.USER_PROFILE_MODAL_OPEN, e);
   };
   FluxDispatcher.subscribe(constants.ActionTypes.USER_PROFILE_MODAL_OPEN, e);
